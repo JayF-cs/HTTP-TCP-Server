86d1406a77df75b85108d8d703737900630b2f5a
James Fryar <athrowawayj12@gmail.com>
2026-09-07 14:18:54 -0400

    Separated request handling from main function

--- src/main.rs
+++ src/main.rs
@@ -10,36 +10,43 @@ fn main() -> Result<(), Box<dyn std::error::Error>>{
     for stream in listener.incoming(){
         //Get TcpStream instance
         let mut stream = stream.unwrap();
-        
-        //Make a buffer read instance
-        let reader = BufReader::new(&mut stream);
-        
-        //Get start line
-        let request: String = reader.lines().next().unwrap().unwrap();
 
-        let start_line;
-        let cont_len: usize;
-        let html: String;
-
-        match &request[..] {
-            "GET / HTTP/1.1" => {
-                start_line = "HTTP/1.1 200 OK";
-                html = fs::read_to_string("page.html").expect("Failed to read page.html");
-                cont_len = html.len();
-            },
-            _ => {
-                start_line = "HTTP/1.1 404 NOT FOUND";
-                html = fs::read_to_string("error.html").expect("Failed to read error.html");
-                cont_len = html.len();
-            }
-        }
-
-        let message = format!("{start_line}\r\nContent-Length: {cont_len}\r\n\r\n{html}");
-
-
-        stream.write_all(&message.into_bytes())?;
-
+        //Handle the request
+        handle_request(stream);        
     }
+
+    Ok(())
+}
+
+fn handle_request(mut stream: TcpStream) -> Result<(), Box<dyn std::error::Error>>{
+    
+    //Create reader to read the buffer stream
+    let reader = BufReader::new(&mut stream);
+    
+    //Get start line
+    let request: String = reader.lines().next().unwrap().unwrap();
+
+    let start_line;
+    let cont_len: usize;
+    let html: String;
+
+    match &request[..] {
+        "GET / HTTP/1.1" => {
+            start_line = "HTTP/1.1 200 OK";
+            html = fs::read_to_string("page.html").expect("Failed to read page.html");
+            cont_len = html.len();
+        },
+        _ => {
+            start_line = "HTTP/1.1 404 NOT FOUND";
+            html = fs::read_to_string("error.html").expect("Failed to read error.html");
+            cont_len = html.len();
+        }
+    }
+
+    let message = format!("{start_line}\r\nContent-Length: {cont_len}\r\n\r\n{html}");
+
+
+    stream.write_all(&message.into_bytes())?;
 
     Ok(())
 }
